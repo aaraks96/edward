492987cf37f25bf1d7226ecb4c411c73eb8c1fab
srujanpanuganti <panuganti.srujan@gmail.com>
2020-06-19 13:56:04 -0400

    not a major change 	modified:   main.py

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ from src.visualize import display
 from src.Map import Map_plot as map
 # import cv2
 
-M = map(150, 300, 5, 2, 4)
+M = map(150, 300, 5, 15, 4)
 
 E = edward_bot(5,5,30,0, M)
 resolution = 10
 
-Path1 = search([260,120], [10,10],10, M,E,1)
+Path1 = search([200,120], [10,10],10, M,E,1)
 path1 = Path1.A_star()
 
 # D1 = display(Path1.start_position, Path1.goal_position, M.x_min,M.y_min,M.x_max,M.y_max)
